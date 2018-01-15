ce6c837cc542a4ec26f8a4b9f82f4f94475dd5b5
Ryan Roat <ryanroat@gmail.com>
2018-01-15 18:11:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ function takeANumber (katzDeliLine, newCust) {
   l++
   return `Welcome, ${newCust}. You are number ${l} in line.`;
 }
+
+function nowServing (katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return `There is nobody waiting to be served!`;
+  }
+  else {
+    return katzDeliLine.shift();
+  }
+}
